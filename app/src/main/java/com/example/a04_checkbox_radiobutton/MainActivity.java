2a79f477c4f6b7985e9b8c59e6ab3e93f1e7a135
Diana Rebeca Ortega <diana.os@tecjerez>
2025-05-14 01:15:38 -0600

    1º commit: GUI creada

---
+++ app/src/main/java/com/example/a04_checkbox_radiobutton/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.a04_checkbox_radiobutton;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+public class MainActivity extends AppCompatActivity {
+    private Button btnConvertir;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        btnConvertir = findViewById(R.id.btnConvertir);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void convertir (View v ){
+        if(v.getId()==R.id.btnConvertir){
+            Toast.makeText(this,  "Convertir", Toast.LENGTH_LONG ).show();
+        }
+    }
+
+}//Class
